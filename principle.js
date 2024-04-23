b4ce7083460a8236e5bf292fa031a7b8accb5b36
jwodlr10014 <85828104+jwodlr10014@users.noreply.github.com>
2024-04-24 07:20:26 +0900

    Add files via upload

---
+++ principle.js
@@ -0,0 +1,179 @@
+const principles = [
+    {
+      name: "퓨어바닐라 쿠키", 
+      kor:`"평화롭고 소중한 바닐라 왕국, 영원히 지키고 싶어."`, 
+      eng: `"Peaceful and precious Vanilla Kingdom, I want to protect it forever"`,
+      introduction: "자연의 축복을 듬뿍 받은 우유크림과 향긋한 바닐라열매가 들어간 아주 포근한 쿠키. 사르르 부드러워 어디에나 잘 어울리는 바닐라처럼, 친절하고 상냥한 퓨어바닐라 쿠키와는 누구나 친구가 될 수 있다. 작은 잘못은 마음의 눈으로 이해해주고, 고귀한 치유의 빛으로 상처 입은 쿠키와 동물들을 치료해 준다고. 타고난 마법의 힘을 모두를 위해 쓰고 싶다며 단맛이 쏙 빠진 슈가프리로드를 몇 날 며칠 걸으며 수행한 일화는 유명하다. 길 위에서 무엇을 깨달은 것인지, 찬란한 후광과 함께 돌아온 퓨어바닐라 쿠키는 자유와 학문의 도시인 바닐라 왕국을 세워 온 세상의 쿠키들에게 사랑과 평화를 베풀었다. 어둠도 물러날 것 같은 온화한 미소 뒤에, 친구들을 위해 기꺼이 한 몸 바칠 결의가 담겨있는 걸 아는 쿠키는 별로 없다."
+    },
+    {
+      name: "홀리베리 쿠키", 
+      kor:`"함성을 질러라! 나 홀리베리 쿠키가 이곳에 왔다!"`, 
+      eng: `"Blow the horn! Hollyberry Cookie has arrived!"`,
+      introduction: "이 쿠키의 선명한 붉은 빛을 보라. 세상에서 가장 뜨거운 햇빛을 받고 자란 베리나무, 그 중에서도 유독 짙은 빛깔의 열매를 엄선해 넣었다! 그 빛깔부터 어찌나 열정적이고 뜨거운지, 만나는 쿠키마다 진한 우정을 나누지 않고서야 못배기는 쿠키가 되었다고. 겹겹이 홀리 잼을 발라 성스러움이 피어오를 때까지 굽고 또 구운 베리-홀리 방패를 내보이며 모두를 지켜줄 것을 맹세하는 홀리베리 쿠키. 세상에서 가장 단단한 것은 서로 간의 믿음! 작은 실수쯤이야 등 한두 번 팡팡 두드리며 용서해준다! 항상 가장 앞으로 나서는 그 등을 보고 홀리베리 쿠키의 신뢰와 따뜻함에 부응하고 싶어지는 것은, 어쩌면 당연한 것이 아닐까?"
+    },
+    {
+      name: "다크카카오 쿠키", 
+      kor:`"검을 쥐었다면 가벼이 휘두르지 마라!"`, 
+      eng: `"If you hold a sword, don't swing it lightly!"`,
+      introduction: "눈보라가 휘몰아치는 절벽 끝에서 자란 다크카카오 열매를 건조한 겨울 바람에 한 번 더 말렸더니 쓰디쓴 카카오닙스가 되었다. 반죽이 얼어버릴 정도로 추운 곳에서 자라서인지 단맛이라고는 전혀 없이 차갑고 고독해진 쿠키. 평범한 쿠키 셋이 들어야 겨우 들릴 포도잼 초코검을 자유롭게 휘두르는 괴력의 쿠키로, 허공을 한 번 벨 때마다 천둥벼락이 떨어지고 산사태가 일어날 정도라고 한다. 한마디 말보다 행동으로 책임감을 보여주었던 다크카카오 쿠키. 하지만 세상을 슬픔에 잠기게 한 검은 가루 전쟁 이후, 왕성에 틀여박혀 혼자 고독의 시간을 쌓아올리게 되는데… 굳게 얼어붙은 다크카카오 쿠키의 마음이 언젠가 녹아버릴 날은 오게 될까."
+    },
+    {
+      name: "골드치즈 쿠키", 
+      kor:`"반짝이는 것은 모두 짐의 것이니라."`, 
+      eng: `"All that glitters belongs to ME!"`,
+      introduction: "경배하라! 태양을, 숭배하라! 황금의 신을! 황금 치즈 줄기가 흐르는 비옥한 사막 위에 온갖 반짝이는 것들로 신전을 채우고 풍요로움을 누렸던 골드치즈 쿠키, 온몸을 채장한 눈부신 황금 장신구와 과시하는 듯한 말투, 커다랗게 펼쳐지는 날개를 단 숭엄함 그 모습에 쿠키들은 절로 무릎을 끓고 찬사를 읊을 수밖에 없었다고 한다. 한 쪽 손을 까딱하면 저기서 황금 산이 솟아오르고, 다른 손을 까딱하면 여기선 황금 강이 흐르니, 풍요의 신이 여기 있노라! 욕심이 어찌나 많았던지, 보물 뿐만 아니라 왕국민들도 자신의 재산이라고 생각한 쿠키, 자신의 소중한 재산을 위해서라면 아낌없이 베풀었던 이 쿠키는, 끝없이 샘솟는 치즈 퐁듀처럼 왕국을 부유함으로 가득 채웠다. 영원토록 빛나는 황금과 행복해하는 왕국민들이 어우러진 그야말로 황금의 낙원을 이룩할 수 있었다. 그 일만 없었더라면."
+    },
+    {
+      name: "세인트릴리 쿠키", 
+      kor:`"난 이 세계의 진실을 알고 싶었어…"`, 
+      eng: `"I wanted to know the truth of this world..."`,
+      introduction: "달빛만이 빛나던 어느 밤에, 비를 머금고 피어난 순백의 백합꽃을 닮은 쿠키. 차분히 가라앉은 눈을 보고 있노라면 조용하고도 침착하게 쿠키와 세계의 비밀을 탐구하고자 하는 호기심이 비친다. 금방이라도 시들어버릴 듯 가녀린 모습이지만, 모두가 행복하고 안전한 쿠키 세계를 위해서 온 세상을 누비는 그 발걸음에는 한 치의 망설임도 두려움도 없었다고. 간절한 마음으로 잡힐듯 말듯한 진실을 쫓다 이내 마주한 세계의 비밀은… 참혹한 진실에 산산조각난 믿음과 마음은 분노와 원망으로 물들고, 이내 언제 어디서부터 싹트기 시작했는지 모를 깊숙한 어둠을 깨워내고 마는데. 과연 세인트릴리 쿠키는 언젠가 고요한 침묵 속에 잠들어버린 자신의 목소리를 다시 들을 수 있게 될까? 그리고 그 때가 왔을 때, 영원히 사라지지 않을 깊은 절망을 품에 안고도 다시 한 번 어둠이 아닌 빛을 향할 수 있게 될까?"
+    },
+    {
+      name: "바다요정 쿠키", 
+      kor:`"당신은 따뜻한 마음을 가지고 계시군요."`, 
+      eng: `"You have a warm heart."`,
+      introduction: "바다를 벗어나면 얼어버리는 차가운 저주에 걸려있던 바다요정 쿠키. 운명을 거스르려다가 얼어붙어 버렸지만, 쿠키들이 따뜻한 마음을 모아준 덕에 돌아올 수 있었다. 지금은 냉기와 물의 힘으로 쿠키들의 든든한 지원군이 되는 바다요정 쿠키. 쉼 없이 흐르는 머리카락 너머의 눈빛은 아직도 서글프지만, 운명에 맞서려는 생각은 변함없이 강렬하다. 물거품이 되어 사라지지 않도록 손에서 절대 검을 놓는 일이 없기를...."
+    },
+    {
+      name: "달빛술사 쿠키", 
+      kor:`“저를 깨운 분이 당신이군요”`, 
+      eng: `"You're the one who woke me up"`,
+      introduction: "밤하늘을 담은 머리카락을 날리며 꿈꾸듯 달리는 달빛술사 쿠키. 쿠키왕국이 생기기도 전 고대 마법사들의 도시에서 마법사들에 의해 만들어져 마법의 비밀을 전수받았지만, 어떤 이유에선지 마법사들이 이곳을 떠난 이후로 홀로 남겨져있다. 꿈을 꾸고 있을 때 더욱 강해지는 마법 능력으로 오랜 시간 침입자들로부터 도시를 지켜왔지만, 이따금씩 옛 기억이 떠올라 아련해진다. 그럴 때면 꿈속 자신만의 아름다운 세계에서 달친구에게 기대어 별빛 친구들과 그림을 그리고 애달픈 마음을 달래곤 한다."
+    },
+    {
+      name: "서리여왕 쿠키", 
+      kor:`"내 차가운 숨결은 모든 것을 얼려버리지"`, 
+      eng: `"My cold breath freezes everything"`,
+      introduction: "매섭게 몰아치는 눈보라 속 얼음성, 성 안쪽에 홀로 앉아 지팡이를 휘두르는 서리여왕 쿠키는 겨울을 다스리는 여왕과도 같다. 그 모습은 마치 오랜 이야기 속 마법처럼 신비롭지만 동시에 가슴 한 켠을 서늘하게 만드는 냉엄함이 느껴진다고. 오랜 시간 살아오면서 쿠키다운 모습은 모두 얼어붙어 버렸는지, 오로지 자신에게 주어진 역할만을 중시하는 서리여왕 쿠키. 그 역할과 운명을 엿보는 건 이 세계의 비밀을 살짝 들춰본 것이나 다름없다는데...? 쿠키였던 시절의 열정과 소망은 더이상 의미가 없어졌지만 자신이 해야 할 일을 해내야 한다는 마음만은 변하지 않았다. 많은 시간이 흐르고 세상이 변해간대도, 서리여왕 쿠키는 변함없이 얼음성 안에서 반짝이는 생명을 세상으로 인도할 것이다."
+    },
+    {
+      name: "블랙펄 쿠키", 
+      kor:`"어서 와, 나약한 쿠키야. 바스러질 준비는 됐니?"`, 
+      eng: `"Welcome, weak cookie. Are you ready to crumble?"`,
+      introduction: "뿌옇게 번진 안개 속 소용돌이치는 물살, 수면을 가르는 거대한 꼬리에 뒤집히는 파도. 검은 바다의 달콤함도 비명도 모두 삼켜버린다네... 무시무시한 검은 바다의 전설 속 거대한 재앙의 정체는 바로 블랙펄 쿠키. 항해하는 배를 장난감처럼 가지고 노는 것도 모자라, 배들의 잔해를 왕좌 삼아 앉고 부서지는 배를 구경하는 모습은 바다의 잔혹함과 어딘지 닮아 있다. 옛이야기 속 인어는 순수하고 맑은 기운으로 바다를 지켜내지만 어째서인지 검은 바다엔 검푸른 원념만이 남아 불안하게 소용돌이칠 뿐... 어째서 검은 바다의 재해를 자처하는지는 알 수 없지만 블랙펄 쿠키가 군림하기 시작한 뒤부터 검은 바다의 악명은 나날이 높아지고 있다. 쿠키들의 이해도 애정도 바라지 않는 블랙펄 쿠키의 갈증을 채울 수 있는 방법은, 정녕 없는 것일까..."
+    }
+]  
+
+const color =[
+    {
+        circlecolor: "linear-gradient(#FEA330 10%, #FDFBCE)"
+    },
+    {
+        circlecolor: "linear-gradient(#99001F, #ED306D 80%)"
+    },
+    {
+        circlecolor: "linear-gradient(#320428 30%, #980C7A)"
+    },
+    {
+        circlecolor: "linear-gradient(#F88A07, #FDE536)"
+    },
+    {
+        circlecolor: "linear-gradient(#5B8F7A 30%, #F3F5F0)"
+    },
+    {
+        circlecolor: "linear-gradient(#273865 30%, #CBDAEE)"
+    },
+    {
+        circlecolor: "linear-gradient(#E5C68D 30%, #9486A5)"
+    },
+    {
+        circlecolor: "linear-gradient(#263479 30%, #8699E7)"
+    },
+    {
+        circlecolor: "linear-gradient(#040508 30%, #D7E5F7)"
+    }
+]
+
+const color2 =[
+    {
+        namecolor: "#E2C290"
+    },
+    {
+        namecolor: "#F64E6A"
+    },
+    {
+        namecolor: "#BE94B4"
+    },
+    {
+        namecolor: "#E2C290"
+    },
+    {
+        namecolor: "#ACBA9E"
+    },
+    {
+        namecolor: "#D6E4EC"
+    },
+    {
+        namecolor: "#F8EDAB"
+    },
+    {
+        namecolor: "#A5B0E7"
+    },
+    {
+        namecolor: "#FFFFFF"
+    }
+]
+
+const nameTag = document.querySelector("h1.name")
+const korTag = document.querySelector("p.kor")
+const engTag = document.querySelector("p.eng")
+const introductionTag = document.querySelector("p.introduction")
+const circleTag = document.querySelector("div.circle")
+
+const cookieImg = document.querySelector("img.cookie")
+const cookie1Img = document.querySelector("img.cookie-img1")
+const cookie2Img = document.querySelector("img.cookie-img2")
+const cookie3Img = document.querySelector("img.cookie-img3")
+
+const leftButton = document.querySelector("div.btn1")
+const rightButton = document.querySelector("div.btn2")
+
+let currentNumber = 1;
+
+leftButton.addEventListener("click", function(){
+  console.log(currentNumber);
+  currentNumber = currentNumber - 1;
+
+  if(currentNumber <= 0){
+    currentNumber = 9;
+  }
+
+  cookieImg.src = `cookie${currentNumber}.gif`;
+  cookie1Img.src = `cookie${currentNumber}01.jpg`; 
+  cookie2Img.src = `cookie${currentNumber}02.jpg`;
+  cookie3Img.src = `cookie${currentNumber}03.jpg`;
+
+  nameTag.innerHTML = principles[currentNumber - 1].name;
+  nameTag.style.color = color2[currentNumber - 1].namecolor;
+  korTag.innerHTML = principles[currentNumber - 1].kor;
+  engTag.innerHTML = principles[currentNumber - 1].eng;
+  introductionTag.innerHTML = principles[currentNumber - 1].introduction;
+  
+  circleTag.style.background = color[currentNumber - 1].circlecolor
+  circleTag.style.marginLeft = `${currentNumber * 15 + "px"}`
+  
+});
+
+
+rightButton.addEventListener("click", function(){
+    console.log(currentNumber);
+    currentNumber = currentNumber + 1;
+  
+    if(currentNumber > 9){
+      currentNumber = 1;
+    }  
+  cookieImg.src = `cookie${currentNumber}.gif`;
+  cookie1Img.src = `cookie${currentNumber}01.jpg`; 
+  cookie2Img.src = `cookie${currentNumber}02.jpg`;
+  cookie3Img.src = `cookie${currentNumber}03.jpg`;
+
+  nameTag.innerHTML = principles[currentNumber - 1].name;
+  nameTag.style.color = color2[currentNumber - 1].namecolor;
+  korTag.innerHTML = principles[currentNumber - 1].kor;
+  engTag.innerHTML = principles[currentNumber- 1 ].eng;
+  introductionTag.innerHTML = principles[currentNumber - 1].introduction;
+  
+  circleTag.style.background = color[currentNumber - 1].circlecolor
+  circleTag.style.marginLeft = `${currentNumber * 15 + "px"}`
+});
